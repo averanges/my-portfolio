e7630e0b87dc0db5f21188a99b89ccebc2f2d468
averanges <nick.belichenko@gmail.com>
2023-05-14 19:08:29 +0900

    Config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {NEXT_PUBLIC_GOOGLE_MAPS_API_KEY : process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
 }
 
 module.exports = nextConfig
