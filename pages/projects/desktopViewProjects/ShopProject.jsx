eff215986a851d77a1f33cec9fa7a839b4035fd1
averanges <nick.belichenko@gmail.com>
2023-05-14 18:31:19 +0900

    Update link url

--- pages/projects/desktopViewProjects/ShopProject.jsx
+++ pages/projects/desktopViewProjects/ShopProject.jsx
@@ -60,7 +60,7 @@ const ShopProject = () => {
                         <FaGithub />
                       </div>
                     </a>
-                    <Link href='https://tamkar-beauty.onrender.com'
+                    <Link href='https://tamkar-beauty.vercel.app'
                     className='rounded-full shadow-lg shadow-gray-400 hover:bg-amber-200 hover:shadow-amber-500 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
--- pages/projects/mobileViewProjects/MobileShopProject.jsx
+++ pages/projects/mobileViewProjects/MobileShopProject.jsx
@@ -56,7 +56,7 @@ const MobileShopProject = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                 </svg>
               </div>
-              <Link href='https://tamkar-beauty.onrender.com' 
+              <Link href='https://tamkar-beauty.vercel.app' 
               className='uppercase'>view</Link>
             </div>
         </div>
