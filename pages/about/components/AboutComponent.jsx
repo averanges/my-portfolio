55c6a5a4ec41e219ab08f368b85a34104853b017
averanges <nick.belichenko@gmail.com>
2024-01-24 18:30:26 +0900

    Update about page

--- pages/about/components/AboutComponent.jsx
+++ pages/about/components/AboutComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Me from '../../../public/assets/nick.jpeg'
+import Me from '../../../public/assets/avatarPhoto2.png'
 import { useSelector } from 'react-redux';
 import { about, firstText, greeting, secondText, skillsFirst, skillsSecond, skillsThird, whoAmI } from '../../../utils/languageChangeAbout';
 import TextTitle from '../../../components/TextTitle';
@@ -13,9 +13,9 @@ const AboutComponent = () => {
         <div className='max-w-[1240px] flex flex-col h-full w-full items-center md:items-start px-10 md:p-10 gap-10 justify-center md:justify-start'>
        <TextTitle title={about}/>
         <div className='flex gap-20 flex-col md:flex-row items-center md:items-start'>
-          <div className='h-fit relative duration-500 w-fit group'>
-              <Image src={Me} width={350} height={300} alt='profile'/>
-              <div className='w-full h-full border-2 border-amber-300 absolute -z-10 top-5 left-5 md:group-hover:top-10 md:group-hover:left-10 duration-500'/>
+          <div className=' relative duration-500 group bg-white  w-[400px] h-[400px] shadow-2xl rounded-3xl'>
+              <Image src={Me} className='object-cover h-full w-full' alt='profile'/>
+              <div className='w-full h-full border-2 rounded-3xl border-amber-300 absolute -z-10 top-5 left-5 md:group-hover:top-10 md:group-hover:left-10 duration-500'/>
           </div>
           <div className='flex-1 flex flex-col gap-5'>
             <h2>{greeting[lang]}</h2>
